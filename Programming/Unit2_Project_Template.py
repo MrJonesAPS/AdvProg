202ce87103fb55b349382431696634fc1fc63790
Mr Jones <christopher.jones@apsva.us>
2023-10-20 12:41:45 -0400

    Adding unit2 project

---
+++ Programming/Unit2_Project_Template.py
@@ -0,0 +1,20 @@
+###############################
+# Program Name: 
+# Written By:
+# Date: 
+# Purpose:
+#
+#
+# Sample 1
+# 
+#   - Inputs
+#
+#   - Output
+#
+# Sample 2
+# 
+#   - Inputs
+#
+#   - Output
+#
+################################
